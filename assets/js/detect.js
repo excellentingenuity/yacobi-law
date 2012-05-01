340753bc4019e0c7ef36907735ac519546a0df2a
James Johnson <James.Johnson@excellentingenuity.com>
2012-05-01 00:38:24 -0400

    v 1.8.6 pages built except for copy in FAQ.html and email on contact form is in test mode sending to excellent ingenuity
    Signed-off-by: James Johnson <James.Johnson@excellentingenuity.com>

--- assets/js/detect.js
+++ assets/js/detect.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 $(document).ready(function() {
- 	if ((window.screen.width<800)||(window.innerWidth<800)) {
+ 	if ((window.screen.width<760)||(window.innerWidth<800)) {
 		//alert('Screen size: smaller than 800x600');
 		document.getElementById("basecss").setAttribute('href', 'css/mobile.css');
 		document.getElementById('top_nav').className=("nav nav-pills nav-stacked");	
